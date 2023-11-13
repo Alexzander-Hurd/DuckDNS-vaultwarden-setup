05a45ab74e96bcc9a978226d6ca01b24245c0232
Alexzander Hurd <ajghurd@gmail.com>
2023-11-13 08:41:58 +0000

    initial commit with swap from wg-east to vw

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
-# DuckDNS-wg-easy-proxy
+# DuckDNS-Vaultwarden
 
-Scripts for [DuckDNS](http://www.duckdns.org/) domain to automate nginx reverse proxy for [wg-easy](https://github.com/WeeJeWel/wg-easy). Uses [acme.sh](https://github.com/acmesh-official/acme.sh) (for certificate generation and automatic renewal) and docker compose. 
+Fork of [DuckDNS-wg-easy-proxy](https://github.com/ad84/DuckDNS-wg-easy-proxy) replacing wg-easy with vaultwarden
+
+Scripts for [DuckDNS](http://www.duckdns.org/) domain to automate nginx reverse proxy for [vaultwarden](https://github.com/dani-garcia/vaultwarden). Uses [acme.sh](https://github.com/acmesh-official/acme.sh) (for certificate generation and automatic renewal) and docker compose. 
 
 ## How to use:
 
 
 1. clone this repo
  ```
- git clone https://github.com/ad84/DuckDNS-wg-easy-proxy.git
+ git clone https://github.com/Alexzander-Hurd/DuckDNS-vaultwarden-setup
  ```
 
 2. `cd` into the project directory, rename `example.env` to `.env` and change the default values & others as required.
@@ -16,15 +18,7 @@ Scripts for [DuckDNS](http://www.duckdns.org/) domain to automate nginx reverse 
 ```
 sudo ./setup.sh
 ```
-4. the wg-easy admin page will now be available at https://vpn.yourduckdomain.duckdns.org, everything else will return 404. The ssl certificates will auto-renew every 60 days. 
-
-## Setting up auth for the admin page with ssl client certificate
-1. run ssl-client.sh 
-```
-sudo ./ssl-client.sh
-```
-2. Now to gain access to the web admin you must now install the client (WG_ADMIN_NAME) pxf file into your OS/browser on the client device (as well as the enter the password for wg-easy). The pfx file will be in the project root. Copy (scp, rysnc etc.) to you device. The pfx file can be reused on as many devices as required and is valid for one year. (By which time i may have written a renewal script :) ) 
-
+4. the vaultwarden page will now be available at https://vault.yourduckdomain.duckdns.org, everything else will return 404. The ssl certificates will auto-renew every 60 days. 
 
 
 
--- setup.sh
+++ setup.sh
@@ -12,29 +12,29 @@ export $(grep -v '^#' .env | xargs -d '\n') # import .env variables
 
 
 echo -e  "${NC}"
-echo -e "Getting, and installing certificates (and setting auto-renewals) for ${GREEN}$WG_SERVER_HOST, vpn.$WG_SERVER_HOST & *.$WG_SERVER_HOST"
+echo -e "Getting, and installing certificates (and setting auto-renewals) for ${GREEN}$SERVER_HOST, .$SERVER_HOST & *.$SERVER_HOST"
 echo -e  "${NC}This may take a little time ."
 sleep 5
 
-#vpn.yourdomain.duckdns.org
-docker exec acme.sh --issue --dns dns_duckdns --insecure -d vpn.$WG_SERVER_HOST -k ec-256 --server letsencrypt
+#vault.yourdomain.duckdns.org
+docker exec acme.sh --issue --dns dns_duckdns --insecure -d vault.$SERVER_HOST -k ec-256 --server letsencrypt
 
-docker exec -e DEPLOY_DOCKER_CONTAINER_LABEL=sh.acme.autoload.domain=$WG_SERVER_HOST -e DEPLOY_DOCKER_CONTAINER_KEY_FILE="/etc/nginx/ssl/vpn.$WG_SERVER_HOST/key.pem" -e DEPLOY_DOCKER_CONTAINER_FULLCHAIN_FILE="/etc/nginx/ssl/vpn.$WG_SERVER_HOST/full.pem" -e DEPLOY_DOCKER_CONTAINER_RELOAD_CMD="nginx -s reload" acme.sh --deploy -d vpn.$WG_SERVER_HOST --ecc --deploy-hook docker
+docker exec -e DEPLOY_DOCKER_CONTAINER_LABEL=sh.acme.autoload.domain=$SERVER_HOST -e DEPLOY_DOCKER_CONTAINER_KEY_FILE="/etc/nginx/ssl/vault.$SERVER_HOST/key.pem" -e DEPLOY_DOCKER_CONTAINER_FULLCHAIN_FILE="/etc/nginx/ssl/vault.$SERVER_HOST/full.pem" -e DEPLOY_DOCKER_CONTAINER_RELOAD_CMD="nginx -s reload" acme.sh --deploy -d vault.$SERVER_HOST --ecc --deploy-hook docker
 
 # main domain for verification - duckdns has same txt record for all subdomains, this way domain is already verified when we make the wildcard later 
-docker exec acme.sh --issue --dns dns_duckdns --insecure -d $WG_SERVER_HOST -k ec-256  --server  letsencrypt
-docker exec -e DEPLOY_DOCKER_CONTAINER_LABEL=sh.acme.autoload.domain=$WG_SERVER_HOST -e DEPLOY_DOCKER_CONTAINER_KEY_FILE="/etc/nginx/ssl/$WG_SERVER_HOST/key.pem" -e DEPLOY_DOCKER_CONTAINER_FULLCHAIN_FILE="/etc/nginx/ssl/$WG_SERVER_HOST/full.pem" -e DEPLOY_DOCKER_CONTAINER_RELOAD_CMD="nginx -s reload" acme.sh --deploy -d $WG_SERVER_HOST --ecc --deploy-hook docker
+docker exec acme.sh --issue --dns dns_duckdns --insecure -d $SERVER_HOST -k ec-256  --server  letsencrypt
+docker exec -e DEPLOY_DOCKER_CONTAINER_LABEL=sh.acme.autoload.domain=$SERVER_HOST -e DEPLOY_DOCKER_CONTAINER_KEY_FILE="/etc/nginx/ssl/$SERVER_HOST/key.pem" -e DEPLOY_DOCKER_CONTAINER_FULLCHAIN_FILE="/etc/nginx/ssl/$SERVER_HOST/full.pem" -e DEPLOY_DOCKER_CONTAINER_RELOAD_CMD="nginx -s reload" acme.sh --deploy -d $SERVER_HOST --ecc --deploy-hook docker
 
 # wild card cert 
-docker exec acme.sh --issue --dns dns_duckdns --insecure -d *.$WG_SERVER_HOST -d $WG_SERVER_HOST -k ec-256  --server  letsencrypt
-docker exec -e DEPLOY_DOCKER_CONTAINER_LABEL=sh.acme.autoload.domain=$WG_SERVER_HOST -e DEPLOY_DOCKER_CONTAINER_KEY_FILE="/etc/nginx/ssl/wildcard.$WG_SERVER_HOST/key.pem" -e DEPLOY_DOCKER_CONTAINER_FULLCHAIN_FILE="/etc/nginx/ssl/wildcard.$WG_SERVER_HOST/full.pem" -e DEPLOY_DOCKER_CONTAINER_RELOAD_CMD="nginx -s reload" acme.sh --deploy -d *.$WG_SERVER_HOST --ecc --deploy-hook docker
+docker exec acme.sh --issue --dns dns_duckdns --insecure -d *.$SERVER_HOST -d $SERVER_HOST -k ec-256  --server  letsencrypt
+docker exec -e DEPLOY_DOCKER_CONTAINER_LABEL=sh.acme.autoload.domain=$SERVER_HOST -e DEPLOY_DOCKER_CONTAINER_KEY_FILE="/etc/nginx/ssl/wildcard.$SERVER_HOST/key.pem" -e DEPLOY_DOCKER_CONTAINER_FULLCHAIN_FILE="/etc/nginx/ssl/wildcard.$SERVER_HOST/full.pem" -e DEPLOY_DOCKER_CONTAINER_RELOAD_CMD="nginx -s reload" acme.sh --deploy -d *.$SERVER_HOST --ecc --deploy-hook docker
 echo -e ""
 docker exec nginx sh -c 'mv /etc/nginx/conf.d/default-server.conf.bak /etc/nginx/conf.d/default-server.conf'
-docker exec nginx sh -c 'mv /etc/nginx/conf.d/wg-easy.conf.bak /etc/nginx/conf.d/wg-easy.conf'
+docker exec nginx sh -c 'mv /etc/nginx/conf.d/vw.conf.bak /etc/nginx/conf.d/vw.conf'
 docker exec nginx sh -c 'nginx -s reload'
 
 echo -e "${GREEN}All done."
-echo -e "${NC}The wg-easy web-admin is avaible at https://vpn.${WG_SERVER_HOST} "
+echo -e "${NC}The vaultwarden web interface is avaible at https://vault.${SERVER_HOST} "
 
 
 
